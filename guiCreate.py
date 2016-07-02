fee0f7fccd8418dfc9933d2f5539f32c58fd36fc
Wilmer Henao <wilmer@umich.edu>
2016-07-02 12:45:36 -0500

    Problems witht the text entry not running

--- guiCreate.py
+++ guiCreate.py
@@ -3,7 +3,9 @@ from tkinter import *
 def runSimplex():
     global e
     string = e.get()
-    print(string)
+    print(string, var)
+    global cs
+    print(cs.get())
 
 root = Tk()
 
@@ -20,9 +22,19 @@ var.set(1)
 
 # Entry that gets the input
 e = StringVar()
-e = Entry(root)
+e = Entry(root, width=40)
 e.pack()
 e.focus_set()
+
+# label above the screen that asks for info
+Label(root, text="Type your constraints below:").pack(side=TOP, padx=15, pady=10)
+
+# Entry that gets the constraints
+# Entry that gets the input
+cs = StringVar()
+cs = Text(root, height=10, width=50)
+cs.insert(INSERT, diary)
+cs.pack()
 
 b = Button(root,text='Run Simplex',command=runSimplex)
 b.pack(side='bottom')
