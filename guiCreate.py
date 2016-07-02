6ca9fd4b1a8cfecb136e6203e48dde1e37566111
Wilmer Henao <wilmer@umich.edu>
2016-07-02 12:26:48 -0500

    maximize minimize buttons and variable input receiver

--- guiCreate.py
+++ guiCreate.py
@@ -9,6 +9,17 @@ root = Tk()
 
 root.title('Name')
 
+# label above the screen that asks for info
+Label(root, text="Input the objective function typing variables x1,x2,...:").pack(side=TOP, padx=15, pady=10)
+
+# Radio button to capture the max, min option
+var = IntVar()
+for text, value in [('Minimize', 1), ('Maximize', 2)]:
+    Radiobutton(root, text=text, value=value, variable=var).pack(anchor=W)
+var.set(1)
+
+# Entry that gets the input
+e = StringVar()
 e = Entry(root)
 e.pack()
 e.focus_set()
